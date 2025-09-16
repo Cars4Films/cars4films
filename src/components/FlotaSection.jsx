d646d1d77341f6ffdb3780c7c9dd6bd5fa670089
xarlizard <38315638+xarlizard@users.noreply.github.com>
2025-09-16 20:03:33 +0200

    fixed routes

--- src/components/FlotaSection.jsx
+++ src/components/FlotaSection.jsx
@@ -1,5 +1,5 @@
 import { useTranslation } from "react-i18next";
-import VehicleCard from "@/VehicleCard";
+import VehicleCard from "@/components/VehicleCard";
 
 const FlotaSection = () => {
   const { t } = useTranslation();
