8c5969da57319439e2c3b29e7834b0383a5ea297
xarlizard <38315638+xarlizard@users.noreply.github.com>
2025-09-16 20:28:47 +0200

    new index for assets with global exports

---
+++ src/assets/index.js
@@ -0,0 +1,66 @@
+// Import all images here for better handling with Vite
+// Vehicle images
+import clasico from './img/clasico.jpg';
+import clasico2 from './img/clasico2.jpg';
+import clasico3 from './img/clasico3.jpg';
+import clasico4 from './img/clasico4.jpg';
+import clasico5 from './img/clasico5.jpg';
+import deportivo from './img/deportivo.jpeg';
+import deportivo2 from './img/deportivo2.jpg';
+import deportivo3 from './img/deportivo3.webp';
+import deportivo3b from './img/deportivo3b.webp';
+import deportivo4 from './img/deportivo4.webp';
+import deportivo5 from './img/deportivo5.jpg';
+import furgo from './img/furgo.jpg';
+import lowloader from './img/lowloader.jpg';
+import bomberos5 from './img/bomberos5.jpg';
+import policia from './img/policia.jpg';
+import policia2 from './img/policia2.jpg';
+import policia3 from './img/policia3.jpg';
+import policia4 from './img/policia4.jpg';
+
+// Technical and service images
+import camaracar from './img/camaracar.jpg';
+import motocam from './img/motocam.jpg';
+import precision from './img/precision.jpg';
+import precision2 from './img/precision2.png';
+import carcare from './img/carcare.png';
+import carprosolutions from './img/carprosolutions.jpg';
+import camionBomberos from './img/camion-bomberos-preventivos-3.jpg';
+
+// People images
+import marcos from './img/marcos.png';
+import marcos1 from './img/marcos1.png';
+import marcosJpeg from './img/marcos.JPEG';
+
+// Logo images
+import logo from './img/logo.png';
+import logoJpeg from './img/logo.jpeg';
+import logoMini1 from './img/logo_mini_1.png';
+import logoMini2 from './img/logo_mini_2.png';
+
+// Videos
+import porsche from './videos/porsche.mp4';
+import porsche2 from './videos/porsche2.mp4';
+
+// Export all assets
+export {
+  // Vehicle images
+  clasico, clasico2, clasico3, clasico4, clasico5,
+  deportivo, deportivo2, deportivo3, deportivo3b, deportivo4, deportivo5,
+  furgo, lowloader, bomberos5,
+  policia, policia2, policia3, policia4,
+  
+  // Technical and service images
+  camaracar, motocam, precision, precision2,
+  carcare, carprosolutions, camionBomberos,
+  
+  // People images
+  marcos, marcos1, marcosJpeg,
+  
+  // Logo images
+  logo, logoJpeg, logoMini1, logoMini2,
+  
+  // Videos
+  porsche, porsche2
+};
